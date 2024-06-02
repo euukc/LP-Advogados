70c3cd2e2bc431aafaf35d7dce325bac57be6504
euukc <keniacarolineferreira@gmail.com>
2024-06-02 11:02:01 -0300

    Style: Inclusao e ajustes no Footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return (
+        <footer className="text-white text-[12px] p-2 flex justify-center gap-32 items-center bg-[#382c1e]">
+            <span>Todos os direitos reservados</span>
+            <span>Desenvolvido por <a href="https://www.instagram.com/euukc/" target="_blank">Kênia Caroline Dev</a></span>
+        </footer>
+    )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Introduction from "./components/Introduction";
 import About from "./components/About";
 import Areas from "./components/Areas";
 import Contato from "./components/Contato";
+import Footer from "./components/Footer";
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <About />
       <Areas/>
       <Contato />
+      <Footer/>
 
     </main>
   );
